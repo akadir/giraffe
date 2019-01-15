7ccb61647253dad1df77b3e1e5e018c09e14c836
kadir <akadirkkoc@gmail.com>
2019-01-16 01:39:31 +0300

    Add private constructor to hide implicit public one

--- src/main/java/com/kadir/twitterbots/giraffe/util/GiraffeConstants.java
+++ src/main/java/com/kadir/twitterbots/giraffe/util/GiraffeConstants.java
@@ -9,4 +9,7 @@ public class GiraffeConstants {
     public static final String PROPERTIES_FILE_NAME = "giraffe.properties";
     public static final String API_KEYS_PREFIX = "giraffe-";
     public static final String EXPORT_FILE_NAME = "giraffe-export.json";
+
+    private GiraffeConstants() {
+    }
 }
